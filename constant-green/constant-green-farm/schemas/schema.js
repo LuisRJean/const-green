bbfe031167449fe359e469fd213d6fd430fcab59
Luis Jean <ljmontreal1@gmail.com>
2022-08-11 22:52:39 -0400

    Update schema.js

--- constant-green/constant-green-farm/schemas/schema.js
+++ constant-green/constant-green-farm/schemas/schema.js
@@ -14,11 +14,15 @@ import localeString from './locale/String'
 import localeText from './locale/Text'
 import localeBlockContent from './locale/BlockContent'
 
-
+// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
+  // We name our schema
   name: 'default',
-  types: schemaTypes.concat([ product, banner ]),
-
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    // The following are document types which will appear
+    // in the studio.
     product,
     vendor,
     category,
