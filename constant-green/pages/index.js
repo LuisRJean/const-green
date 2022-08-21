e5c15e5171dce0743cdfe6582d1d47cb6252e3ba
Luis Jean <ljmontreal1@gmail.com>
2022-08-21 18:28:59 -0400

    first commits

--- constant-green/pages/index.js
+++ constant-green/pages/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      HeroBanner
 
+      <div className="products-heading">
+        <h2>Best selling microgreens</h2>
+        <p>Range of greens</p>
+      </div>
+
+      <div className="products-container">
+        {['PRODUCT 1', 'PRODUCT 2'].map((product) => product)}
+      </div>
+
+      FooterBanner
+      </div>
+  );
+  }
 export default index
-
