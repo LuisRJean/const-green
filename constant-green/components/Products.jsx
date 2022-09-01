7e6c6c3549341913173627a29e99a838c0ad15f2
Luis Jean <ljmontreal1@gmail.com>
2022-09-01 17:19:14 -0400

    Update Products.jsx

--- constant-green/components/Products.jsx
+++ constant-green/components/Products.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
+import { urlFor } from '../constant-green-farm/lib/client';
 
-const Products = () => {
+const Products = ({ Products: image, name, slug, price }) => {
   return (
     <div>
-        
+
     </div>
   )
 }
