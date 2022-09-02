6a49dcb050085bad7ae91ba0bb9bd1c464cddc4f
Luis Jean <ljmontreal1@gmail.com>
2022-09-02 18:57:43 -0400

    Update HeroBanner.jsx

--- constant-green/components/HeroBanner.jsx
+++ constant-green/components/HeroBanner.jsx
@@ -9,7 +9,7 @@ const HeroBanner = ({ heroBanner }) => {
         <p className="broccoli">sunflowers</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <img src={urlFor(heroBanner.image)} alt="sunflower" className="hero-banner-image" />
 
 
         <div>
