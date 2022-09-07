c27d710a119e05a266effdec1ce4267655407d83
Luis Jean <ljmontreal1@gmail.com>
2022-09-07 17:46:09 -0400

    Update [slug].js

--- constant-green/pages/product/[slug].js
+++ constant-green/pages/product/[slug].js
@@ -20,22 +20,29 @@ const ProductDetails = ({ product, products }) => {
 
 export const getStaticPaths = async () => {
     const query = `*[_type == "product"] {
-        slug {
-            current
-        }
+    slug {
+      current
     }
-    `
-}    
+  }
+  `;
 
-const products = await client.fetch(query);
+    const products = await client.fetch(query);
 
-const paths = products.map((product) => ({
-    params: { slug: product.slug.current }
-}))
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
 
-export const getStaticProps = async ({ params: { slug }}) => {
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
     const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-    const productQuery = '*[_type == "product"]'
+    const productsQuery = '*[_type == "product"]'
 
     const product = await client.fetch(query);
     const products = await client.fetch(productsQuery);
@@ -46,4 +53,5 @@ export const getStaticProps = async ({ params: { slug }}) => {
         props: { products, product }
     }
 }
+
 export default ProductDetails
