31759a72f220a97a938ae24b8d3b950a7fc7d638
Luis Jean <ljmontreal1@gmail.com>
2022-09-07 17:18:54 -0400

    Update [slug].js

--- constant-green/pages/product/[slug].js
+++ constant-green/pages/product/[slug].js
@@ -16,4 +16,15 @@ const ProductDetails = () => {
   )
 }
 
+export const getStaticProps = async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+        props: { products, bannerData }
+    }
+}
 export default ProductDetails
