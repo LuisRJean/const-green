5a3e9e33bc2e665afc41167503e2f7e808910473
Luis Jean <ljmontreal1@gmail.com>
2022-09-07 16:51:48 -0400

    .

--- constant-green/components/Navbar.jsx
+++ constant-green/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import Link from 'next/link';
+import { AiOutlineShop } from 'react-icons/ai';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Constant Green</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShop />
+        <span className="cart-items-qty">0</span>
+      </button>
+      </div>
   )
 }
 
