830d44d90a15a3a6e6a3c6b0f0a1ae645ea5aab6
Luis Jean <ljmontreal1@gmail.com>
2022-09-07 20:36:36 -0400

    Update [slug].js

--- constant-green/pages/product/[slug].js
+++ constant-green/pages/product/[slug].js
@@ -67,6 +67,12 @@ const ProductDetails = ({ product, products }) => {
                         </div>
                 </div>
             </div>
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container"></div>
+                </div>
+            </div>
     </div>
   )
 }
