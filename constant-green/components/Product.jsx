3fded92fa16ee4f6d72186a4d55043a07925d6de
Luis Jean <ljmontreal1@gmail.com>
2022-09-07 21:45:29 -0400

    Update Product.jsx

--- constant-green/components/Product.jsx
+++ constant-green/components/Product.jsx
@@ -9,7 +9,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img 
-          src="https://cdn.sanity.io/images/763jxe6w/production/ac723f1630169acfef668141685c71e8902fe3d2-512x288.webp"
+            src="https://cdn.sanity.io/images/763jxe6w/production/ac723f1630169acfef668141685c71e8902fe3d2-512x288.webp" 
             width={250}
             height={250}
             className="product-image"
