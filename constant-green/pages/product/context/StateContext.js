96ff2663397ec12a4fcc80654936aa533005f04a
Luis Jean <ljmontreal1@gmail.com>
2022-09-10 15:20:23 -0400

    Update StateContext.js

--- constant-green/pages/product/context/StateContext.js
+++ constant-green/pages/product/context/StateContext.js
@@ -6,10 +6,11 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalPrice, setTotalPrice] = useState([]);
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     let foundProduct;
     let index;
 }
+
