b09a37640dbb2a63317705814d6e0ecbaaf891c4
Luis Jean <ljmontreal1@gmail.com>
2022-09-11 09:22:21 -0400

    context

--- constant-green/pages/product/context/StateContext.js
+++ constant-green/pages/product/context/StateContext.js
@@ -6,13 +6,13 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState(0);
+    const [totalPrice, setTotalPrice] = useState([]);
+    const [totalQuantities, setTotalQuantities] = useState([]);
     const [qty, setQty] = useState(1);
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
-
+    
     const decQty = () => {
         setQty((prevQty) => {
             if (prevQty - 1 < 1) return 1;
@@ -37,3 +37,5 @@ export const StateContext = ({ children }) => {
     )
 }
 }
+
+export const useStateContext = () => useContext(Context);
