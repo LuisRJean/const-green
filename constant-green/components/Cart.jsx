326ecb2b21fe7cf6346f2dfc0920f60e5edc7769
Luis Jean <ljmontreal1@gmail.com>
2022-09-13 16:50:17 -0400

    Update Cart.jsx

--- constant-green/components/Cart.jsx
+++ constant-green/components/Cart.jsx
@@ -2,6 +2,9 @@ import React, { useRef } from 'react';
 import Link from 'next/link';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+
+
 
 
 const Cart = () => {
