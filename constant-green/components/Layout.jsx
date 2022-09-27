570a01ba047f2229fd4dc4917804e7c1e8209003
Luis Jean <ljmontreal1@gmail.com>
2022-09-27 19:40:19 -0400

    Update Layout.jsx

--- constant-green/components/Layout.jsx
+++ constant-green/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Luis Store</title>
+        <title>Constant Microgreens</title>
         </Head>
         <header>
           <Navbar />
