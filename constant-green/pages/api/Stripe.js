e253502a4a297a1613c1beff8c05b8831b0a7277
Luis Jean <ljmontreal1@gmail.com>
2022-09-27 18:16:33 -0400

    Update Stripe.js

--- constant-green/pages/api/Stripe.js
+++ constant-green/pages/api/Stripe.js
@@ -2,4 +2,11 @@ import Stripe from 'stripe';
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) 
+export default async function handler(req, res) {
+
+
+
+
+
+    
+}
