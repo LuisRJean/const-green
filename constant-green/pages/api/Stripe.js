51905022975e016e19b64cf97e647d3fe4e948b2
Luis Jean <ljmontreal1@gmail.com>
2022-09-27 18:18:44 -0400

    Update Stripe.js

--- constant-green/pages/api/Stripe.js
+++ constant-green/pages/api/Stripe.js
@@ -3,10 +3,15 @@ import Stripe from 'stripe';
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+    if(req.method === 'POST') {
+        try {
+
+        }
+        catch (error) {
+            res.stauts(500).json({ statusCode: 500, message: error.message });
 
 
+    } 
+    }
 
-
-
-    
 }
