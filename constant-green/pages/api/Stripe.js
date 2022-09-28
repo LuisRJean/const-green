e3d80c3da33834a6634d5984f57680be0690abb8
Luis Jean <ljmontreal1@gmail.com>
2022-09-27 21:27:41 -0400

    Update Stripe.js

--- constant-green/pages/api/Stripe.js
+++ constant-green/pages/api/Stripe.js
@@ -11,11 +11,11 @@ export default async function handler(req, res) {
                 payment_method_types: ['card'],
                 billing_address_collection: 'auto',
                 shipping_options: [
-                    { shipping_rate: 'shr_1Lmo5FFi3LnWynYN1OS2OklG' },
+                    { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
                 ],
                 line_items: req.body.map((item) => {
                     const img = item.image[0].asset._ref;
-                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/763jxe6w/production/').replace('-webp', '.webp');
 
                     return {
                         price_data: {
