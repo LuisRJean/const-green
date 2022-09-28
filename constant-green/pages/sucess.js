26e6359b88daae9371fd5a9382a50ec64eb04a6a
Luis Jean <ljmontreal1@gmail.com>
2022-09-28 19:40:12 -0400

    Update sucess.js

--- constant-green/pages/sucess.js
+++ constant-green/pages/sucess.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+import { useRouter } from 'next/router';
+
+import { useStateContext } from '../../pages/product/context/StateContext';
 
 const sucess = () => {
+    const { setCartItems, setTotalPrice, setTotalQuantites } = useStateContext();
+    const [order, setOrder] = useState(null);
+
   return (
-    <div>
+    <div className="success-wrapper">
+        <div className="success">
+            <p className="icon">
+                <BsBagCheckFill />
+            </p>
+            </div>
 
     </div>
   )
