b74c43e4d08fc4a2311619f86a15984c0b912b9e
Luis Jean <ljmontreal1@gmail.com>
2022-09-27 23:04:51 -0400

    Update Stripe.js

--- constant-green/pages/api/Stripe.js
+++ constant-green/pages/api/Stripe.js
@@ -1,11 +1,12 @@
 import Stripe  from 'stripe';
 
-
-
 const stripe = require('Stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
+    console.log(req.body.cartItems)
+
+
         try {
             const params = {
                 submit_type: 'pay',
@@ -18,6 +19,7 @@ export default async function handler(req, res) {
                 line_items: req.body.map((item) => {
                     const img = item.image[0].asset._ref;
                     const newImage = img.replace('image-', 'https://cdn.sanity.io/images/763jxe6w/production/').replace('-webp', '.webp');
+                    console.log('IMAGE', newImage)
 
                     return {
                         price_data: {
