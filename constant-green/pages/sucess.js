b5891c27ac74f898368656706a63182bf59b51f8
Luis Jean <ljmontreal1@gmail.com>
2022-09-28 19:57:28 -0400

    Update sucess.js

--- constant-green/pages/sucess.js
+++ constant-green/pages/sucess.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 
-import { useStateContext } from '../pages/product/context/StateContext';
-import { runFireworks } from '../constant-green-farm/lib/utils';
+import { useStateContext } from '../../pages/product/context/StateContext';
+
 
 const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
@@ -22,6 +22,19 @@ const Success = () => {
                 <p className="icon">
                     <BsBagCheckFill />
                 </p>
+                <h2>Thank you for your order!</h2>
+                <p className="email-msg">Check your email inbox for the receipt.</p>
+                <p className="description">
+                    If you have any questions, please email
+                    <a className="email" href="mailto:order@example.com">
+                        order@example.com
+                    </a>
+                </p>
+                <Link href="/">
+                    <button type="button" width="300px" className="btn">
+                        Continue Shopping
+                    </button>
+                </Link>
             </div>
         </div>
     )
