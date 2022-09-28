6504e261b72b71ab0dbf01c04388341628b2a606
Luis Jean <ljmontreal1@gmail.com>
2022-09-28 19:45:34 -0400

    Update sucess.js

--- constant-green/pages/sucess.js
+++ constant-green/pages/sucess.js
@@ -22,19 +22,8 @@ const Success = () => {
                 <p className="icon">
                     <BsBagCheckFill />
                 </p>
-                <h2>Thank you for your order!</h2>
-                <p className="email-msg">Check your email inbox for the receipt.</p>
-                <p className="description">
-                    If you have any questions, please email
-                    <a className="email" href="mailto:order@example.com">
-                        order@example.com
-                    </a>
-                </p>
-                <Link href="/">
-                    <button type="button" width="300px" className="btn">
-                        Continue Shopping
-                    </button>
-                </Link>
+                
+                
             </div>
         </div>
     )
