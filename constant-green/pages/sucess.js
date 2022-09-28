95141cd6decbb3d8b7fedc87cb16fb200fd39000
Luis Jean <ljmontreal1@gmail.com>
2022-09-28 19:42:57 -0400

    Update sucess.js

--- constant-green/pages/sucess.js
+++ constant-green/pages/sucess.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 
 import { useStateContext } from '../../pages/product/context/StateContext';
 
-const Success = () => {
+const success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantites } = useStateContext();
     const [order, setOrder] = useState(null);
 
