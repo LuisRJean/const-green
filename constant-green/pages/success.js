f72eef2ad0d2320703771e5ebfaf4ae3f394d38c
Luis Jean <ljmontreal1@gmail.com>
2022-09-28 23:24:29 -0400

    first commit

--- constant-green/pages/success.js
+++ constant-green/pages/success.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
-
 import { useStateContext } from './product/context/StateContext';
+
 import { runFireworks } from '../constant-green-farm/lib/utils';
 
 
